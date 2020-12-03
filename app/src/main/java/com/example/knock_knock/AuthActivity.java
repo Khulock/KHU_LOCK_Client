b9d21894a1247b256709d0d55228b459f47d1ca1
Taewoo Kang <taewookang96@gmail.com>
2020-12-04 00:37:15 +0900

    [MERGE] bio auth service

--- app/src/main/java/com/example/knock_knock/AuthActivity.java
+++ app/src/main/java/com/example/knock_knock/AuthActivity.java
@@ -2,7 +2,6 @@ package com.example.knock_knock;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.biometric.BiometricPrompt;
 import androidx.core.content.ContextCompat;
 
 import android.content.Context;
@@ -17,12 +16,6 @@ import com.example.knock_knock.Service.AuthManager;
 import java.util.concurrent.Executor;
 
 public class AuthActivity extends AppCompatActivity {
-
-    private Context mContext;
-    private Executor mMainExecutor;
-    private BiometricPrompt mBiometricPrompt;
-    private BiometricPrompt.PromptInfo mBiometricPromptInfo;
-    private AuthManager authManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
