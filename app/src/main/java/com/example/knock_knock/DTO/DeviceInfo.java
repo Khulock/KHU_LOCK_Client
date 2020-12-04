0999f4bc8f0e9a11866f4658bb20f2cf6d905664
Taewoo Kang <taewookang96@gmail.com>
2020-12-05 01:01:10 +0900

    [SUCCESS] device list recycler view

---
+++ app/src/main/java/com/example/knock_knock/DTO/DeviceInfo.java
@@ -0,0 +1,13 @@
+package com.example.knock_knock.DTO;
+
+public class DeviceInfo {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- app/src/main/java/com/example/knock_knock/HomeFragment.java
+++ app/src/main/java/com/example/knock_knock/HomeFragment.java
@@ -3,11 +3,15 @@ package com.example.knock_knock;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.knock_knock.DTO.DeviceInfo;
+import com.example.knock_knock.Service.DeviceListAdapter;
 import com.example.knock_knock.databinding.FragmentHomeBinding;
 
 import java.util.ArrayList;
@@ -18,7 +22,6 @@ public class HomeFragment extends Fragment {
     private FragmentHomeBinding mBinding;
 
     public HomeFragment() {
-        // Required empty public constructor
     }
 
     @Override
@@ -29,7 +32,6 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         mBinding = FragmentHomeBinding.inflate(inflater, container, false);
         return mBinding.getRoot();
     }
@@ -37,10 +39,19 @@ public class HomeFragment extends Fragment {
     @Override
     public void onStart() {
         super.onStart();
-        List<String> temp = new ArrayList<>();
-        temp.add("hhh");
-        temp.add("BBBB");
-        new ControlDialog(requireContext(), temp).show();
+        List<DeviceInfo> list = new ArrayList<>();
+        DeviceInfo deviceInfo = new DeviceInfo();
+        deviceInfo.setName("AAAAA");
+        list.add(deviceInfo);
 
+        DeviceInfo deviceInfo2 = new DeviceInfo();
+        deviceInfo2.setName("BBBB");
+        list.add(deviceInfo2);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(requireContext());
+        mBinding.recycleDeviceItem.setLayoutManager(layoutManager);
+
+        DeviceListAdapter deviceListAdapter = new DeviceListAdapter(list);
+        mBinding.recycleDeviceItem.setAdapter(deviceListAdapter);
     }
 }
